f2d02d2e30f820bc84f8f065d9f467c490ea559d
Alexander Panko <god@panki.ru>
2017-03-07 22:57:49 +0100

    Guess content type for binary params by filename extension

--- facebook_test.go
+++ facebook_test.go
@@ -14,6 +14,7 @@ import (
 	"fmt"
 	"testing"
 	"time"
+	"strings"
 )
 
 const (
@@ -888,6 +889,27 @@ func TestParamsEncode(t *testing.T) {
 
 	mime, err := params.Encode(buf)
 	t.Logf("complex encode result is '%v'. [e:%v] [mime:%v]", buf.String(), err, mime)
+}
+
+func TestBinaryParamsEncode(t *testing.T) {
+
+	buf := &bytes.Buffer{}
+	params := Params{}
+	params["attachment"] = FileAlias("image.jpg", "LICENSE")
+
+	contentTypeImage := "Content-Type: image/jpeg"
+	if mime, err := params.Encode(buf); err != nil || !strings.Contains(mime, _MIME_FORM_DATA) || !strings.Contains(buf.String(), contentTypeImage) {
+		t.Fatalf("wrong binary params encode result. expected content type is '%v'. actual is '%v'. [e:%v] [mime:%v]", contentTypeImage, buf.String(), err, mime)
+	}
+
+	// Fallback for unknown content types
+	// should be application/octet-stream
+	buf.Reset()
+	params = Params{"attachment": FileAlias("image.unknown", "LICENSE")}
+	contentTypeOctet := "Content-Type: application/octet-stream"
+	if mime, err := params.Encode(buf); err != nil || !strings.Contains(mime, _MIME_FORM_DATA) || !strings.Contains(buf.String(), contentTypeOctet) {
+		t.Fatalf("wrong binary params encode result. expected content type is '%v'. actual is '%v'. [e:%v] [mime:%v]", contentTypeOctet, buf.String(), err, mime)
+	}
 }
 
 func TestStructFieldTag(t *testing.T) {
